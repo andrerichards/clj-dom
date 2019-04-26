9c9e7b8293d690dc23738a95661daf00e9e3efcc
Andre Richards <andrer@hti-systems.com>
2019-04-26 15:18:27 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject defng/clj-dom "0.1.0-SNAPSHOT"
+  :description "Basic Clojure wrapper for JDOM 2"
+  :url "https://github.com/andrerichards/clj-dom"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]])
---
+++ test/defng/clj_dom/core_test.clj
@@ -0,0 +1,7 @@
+(ns defng.clj-dom.core-test
+  (:require [clojure.test :refer :all]
+            [cljdom.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
